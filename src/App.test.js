3ce0c65dfdc9e635d46ea96a6fd72ac2cd35ab76
Rohitwable <rohitwable46@gmail.com>
2020-01-04 20:56:47 +0530

    data-test attribute for test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+test('renders without any error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='app-component']");
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increament button', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='counter-button']");
+  expect(appComponent.length).toBe(1);
+});
+
+test('render counter display', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='counter-display']");
+  expect(appComponent.length).toBe(1);
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('clicking counter button increaments counter display', () => {
+
 });
